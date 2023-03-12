8d6db188587bdeaf58c490cc447484e54d2a3d28
irina p <gorohovairina21@gmail.com>
2023-03-12 21:55:55 +0300

    Initial commit

---
+++ src/mine.java
@@ -0,0 +1,16 @@
+public class mine {
+
+    public static void main(String[] args) {
+        int price = 13676;
+        int mile = 20;
+
+        int bonus;
+        if (price > 0) {
+            bonus = price / mile;
+        } else {
+            bonus = 0;
+        }
+
+        System.out.println("Количество бонусных миль " + bonus);
+    }
+}
